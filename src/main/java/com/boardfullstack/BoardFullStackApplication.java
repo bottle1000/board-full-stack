a35429777939e84a7dd2a131b5cd8d38fc902997
박병천 <genius178896@gmail.com>
2025-11-20 10:45:31 +0900

    chore: 스프링 부트 프로젝트 최초 커밋 with DB Config

---
+++ src/main/java/com/boardfullstack/BoardFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.boardfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardFullStackApplication.class, args);
+    }
+
+}
