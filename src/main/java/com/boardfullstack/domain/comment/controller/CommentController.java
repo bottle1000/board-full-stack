88dc0e8c2006260e1a5c01142d557096921b650e
박병천 <genius178896@gmail.com>
2025-12-22 18:07:01 +0900

    feat : Comment 조회, 삭제 및 헬퍼 함수 추가

--- src/main/java/com/boardfullstack/domain/comment/controller/CommentController.java
+++ src/main/java/com/boardfullstack/domain/comment/controller/CommentController.java
@@ -8,21 +8,34 @@ import com.boardfullstack.global.security.UserPrincipal;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/posts/{postId}")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/api/posts/{postId}/comments")
+    @PostMapping("/comments")
     public ApiResponse<CommentResponse> createComment(@AuthenticationPrincipal UserPrincipal principal,
                                                       @PathVariable Long postId,
                                                       @RequestBody @Valid CommentCreateRequest request) {
         return ApiResponse.ok(commentService.createComment(principal.getId(), postId, request));
+    }
+
+    @GetMapping
+    public ApiResponse<List<CommentResponse>> getComments(@PathVariable Long postId) {
+        return ApiResponse.ok(commentService.getComments(postId));
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public ApiResponse<Void> deleteComment(@PathVariable Long commentId,
+                                           @PathVariable Long postId,
+                                           @AuthenticationPrincipal UserPrincipal principal) {
+        commentService.deleteComment(principal.getId(), postId, commentId);
+        return ApiResponse.ok(null);
     }
 }
--- src/main/java/com/boardfullstack/domain/comment/service/CommentService.java
+++ src/main/java/com/boardfullstack/domain/comment/service/CommentService.java
@@ -8,11 +8,14 @@ import com.boardfullstack.domain.post.entity.Post;
 import com.boardfullstack.domain.post.repository.PostRepository;
 import com.boardfullstack.domain.user.entity.User;
 import com.boardfullstack.domain.user.repository.UserRepository;
+import com.boardfullstack.global.common.response.PagedResponse;
 import com.boardfullstack.global.exception.CustomException;
 import com.boardfullstack.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -26,15 +29,55 @@ public class CommentService {
     @Transactional
     public CommentResponse createComment(Long principalId, Long postId, CommentCreateRequest request) {
 
-        Post post = postRepository.findPostByIdAndDeletedFalse(postId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        Post post = getActivePostOrThrow(postId);
 
-        User user = userRepository.findById(principalId)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        User user = getUserOrThrow(principalId);
 
         Comment comment = Comment.createComment(user, post, request.getContent());
         Comment saved = commentRepository.save(comment);
 
         return CommentResponse.from(saved);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CommentResponse> getComments(Long postId) {
+
+        getActivePostOrThrow(postId);
+
+        return commentRepository.findAllByPostIdAndDeletedFalseOrderByCreatedAtAsc(postId)
+                .stream()
+                .map(CommentResponse::from)
+                .toList();
+    }
+
+    @Transactional
+    public void deleteComment(Long principalId, Long postId , Long commentId) {
+        getActivePostOrThrow(postId);
+        Comment comment = getActiveCommentOrThrow(commentId);
+
+        validateCommentAuthorOrThrow(comment, principalId);
+
+        commentRepository.delete(comment);
+    }
+
+    private Post getActivePostOrThrow(Long postId) {
+        return postRepository.findPostByIdAndDeletedFalse(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+    }
+
+    private User getUserOrThrow(Long principalId) {
+        return userRepository.findById(principalId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private Comment getActiveCommentOrThrow(Long commentId) {
+        return commentRepository.findByIdAndDeletedFalse(commentId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+    }
+
+    private void validateCommentAuthorOrThrow(Comment comment, Long principalId) {
+        if (!comment.getAuthor().getId().equals(principalId)) {
+            throw new CustomException(ErrorCode.COMMENT_FORBIDDEN);
+        }
     }
 }
